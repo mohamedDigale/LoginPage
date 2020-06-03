f9ceb201b619a8e8ef62bff8651f657394212456
mohamedDigale <65135907+mohamedDigale@users.noreply.github.com>
2020-06-02 22:04:06 -0400

    Add files via upload

---
+++ loginPage/script.js
@@ -0,0 +1,7 @@
+var h1=document.createElement('h1');
+   
+    var textAnswer=document.createTextNode('I did step 1');
+    h1.setAttribute('id','finalUnit');
+    h1.appendChild(textAnswer);
+    document.getElementById('trail').appendChild(h1);
+
